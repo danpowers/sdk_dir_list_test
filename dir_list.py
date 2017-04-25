053482b6012e416b39357450e1eaed67a7230612
danpowers <danpowers@users.noreply.github.com>
2017-04-25 10:57:03 -0500

    Update dir_list.py

--- dir_list.py
+++ dir_list.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
+#for ticket 307471
 
 import globus_sdk
+import time
 
 CLIENT_ID = 'b21de7ea-af4c-4458-a71a-6c63eaa4c8f6'
 EP_UUID = ''
@@ -17,15 +19,14 @@ auth_code = get_input(
     'Please enter the code you get after login here: ').strip()
 token_response = client.oauth2_exchange_code_for_tokens(auth_code)
 
-globus_auth_data = token_response.by_resource_server['auth.globus.org']
 globus_transfer_data = token_response.by_resource_server['transfer.api.globus.org']
 
-AUTH_TOKEN = globus_auth_data['access_token']
 TRANSFER_TOKEN = globus_transfer_data['access_token']
 
 authorizer = globus_sdk.AccessTokenAuthorizer(TRANSFER_TOKEN)
 tc = globus_sdk.TransferClient(authorizer=authorizer)
 
-for entry in tc.operation_ls(EP_UUID, path=top_level_dir):
-    line = tc.operation_ls(EP_UUID, path=top_level_dir+entry['name'])
-    print(line)
+for directory in tc.operation_ls(EP_UUID, path=top_level_dir):
+    subdir_list = tc.operation_ls(EP_UUID, path=top_level_dir+directory['name'])
+    print(subdir_list)
+    time.sleep(0.5)
